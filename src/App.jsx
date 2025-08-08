627de16e26ccaec9ecf28d283cc2c7fdf3ef25e5
Bauty6 <diegomax763@gmail.com>
2025-08-08 03:00:51 -0300

    boton aplicado

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import Entrevista from './components/Entrevista';
 import TablaFamilia from './components/Tabla';
 import Anamnesis from './components/Anamnesis';
+import SubmitButton from './components/SubmitButton';
 
 
 
 const App= () => {
   return (
     <div className="App">
-      <h1>Formulario de Entrevista</h1>
+    <h1>Formulario de Entrevista</h1>
       <Entrevista />
       <h2>Grupo Familiar Conviviente</h2>
       <TablaFamilia />
       <h2>Formulario de Anamnesis</h2>
      <Anamnesis/>
+     <submitButton onClick={() => alert("Formulario enviado")}>Enviar</submitButton>
     </div>
   );
 }
